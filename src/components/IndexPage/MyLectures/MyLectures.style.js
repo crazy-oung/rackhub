15fd8cf7a6747e43627dcbc84a94e9fd35e2fe65
crazy_oung <hdayoungh@gmail.com>
2022-12-10 02:16:58 +0900

    release: Test

--- src/components/IndexPage/MyLectures/MyLectures.style.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import {
-  MobileViewWidth,
-  MINIMUM_PC_WEBVIEW_WIDTH,
-  FontColor,
-  MenuColor,
-} from "../../../static/Shared/commonStyles";
